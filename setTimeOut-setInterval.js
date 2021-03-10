e1ea5f0abbaed51d6c47eafd6c18729084eea86a
asraful196 <ahamedsunny14@gmail.com>
2021-03-11 00:48:54 +0600

    setTimeOut vs setInterval

---
+++ setTimeOut-setInterval.js
@@ -0,0 +1,29 @@
+//  ::25-9:Asynchronous javascript setTimeout ,setInterval....
+
+// / setTimeout kivabe kaz kore... sobgola output dekhanor por output dekhabe and time jei time set kora thakbe oi time show korbe
+// / setInterval kivabe kaz kore... sobgola output dekhanor por time onopate bar bat output dekhabe and time jei time set kora thakbe oi time show korbe
+
+
+console.log(0000);
+console.log(5555);
+setTimeout(() => {
+    console.log(2222);
+    }, 3000);
+
+
+    setInterval(() => {
+        console.log(1111);    // output break dewar jonno ctrl+c click korte hobe.
+    }, 5000);
+
+
+console.log(3333);
+console.log(4444);
+
+
+
+
+
+
+
+
+
